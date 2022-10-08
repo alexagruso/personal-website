fb5a9fb339b029c27eeee326414acef3d1f98cc9
Alexander Agruso <agrusoalexander@gmail.com>
2022-10-07 23:13:13 -0500

    setup ejs

---
+++ server.ts
@@ -0,0 +1,16 @@
+import express, { Application } from "express";
+import path from "path";
+
+const app: Application = express();
+const port = parseInt(process.env.PORT || "5000");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
