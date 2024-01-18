8993099d416d89e5a530694d713be882dc45036a
AlexAgruso <agrusoalexander@gmail.com>
2024-01-18 17:39:33 -0600

    text input component

--- src/lib/components/ui-kit/TextInput.svelte
+++ src/lib/components/ui-kit/TextInput.svelte
@@ -1,11 +1,51 @@
 <script lang="ts">
-    export let label: string;
+    export let label = "Label";
     export let name: string;
+    export let placeholder = "Placeholder";
+    export let value: string;
 </script>
 
-<label for={name} class="col">
+<label for="{name}-input" class="col">
     <span>{label}</span>
+    <input type="text" {name} id="{name}-input" {placeholder} bind:value />
 </label>
 
 <style lang="scss">
+    $hover-transition-duration: 150ms;
+
+    label {
+        gap: 1rem;
+    }
+
+    label > span {
+        font-size: 0.8rem;
+        font-weight: bold;
+        text-transform: uppercase;
+    }
+
+    input {
+        transition: border-color $hover-transition-duration;
+
+        border-bottom: 2px solid grey;
+        padding-bottom: 0.25rem;
+
+        font-size: 1rem;
+        font-weight: 100;
+
+        &:hover,
+        &:focus {
+            border-color: black;
+
+            &::placeholder {
+                color: black;
+            }
+        }
+    }
+
+    input::placeholder {
+        transition: color $hover-transition-duration;
+
+        font-size: 1rem;
+        color: grey;
+    }
 </style>
